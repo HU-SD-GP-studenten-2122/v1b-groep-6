1b926227d950b5ade145fb3fc7d44902e11b66d4
Lookie4Fun <94829223+Lookie4Fun@users.noreply.github.com>
2022-04-06 09:40:25 +0200

    Delete ProjectRegistratieSysteem directory

--- ProjectRegistratieSysteem/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.projectregistratiesysteem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.projectregistratiesysteem to javafx.fxml;
-    exports com.example.projectregistratiesysteem;
-}
